38c6eeebdf94d31135e0027b7ac8926cdce60823
a-sh <iam.angie@ymail.com>
2022-08-14 21:43:56 +1000

    WIP Tutorial Part 3

--- final/part-03/final/server/src/resolvers.js
+++ final/part-03/final/server/src/resolvers.js
@@ -1,20 +1,22 @@
 const resolvers = {
   Query: {
-    // returns an array of Tracks that will be used to populate
-    // the homepage grid of our web client
-    // tracksForHome: (parent, args, context, info) => {}
+    // returns an array of Tracks that will be used to populate the homepage grid of our web client
     tracksForHome: (_, __, { dataSources }) => {
       return dataSources.trackAPI.getTracksForHome();
     },
 
+    // get a single track by ID, for the track page
     track: (_, { id }, { dataSources }) => {
       return dataSources.trackAPI.getTrack(id);
     },
   },
-
   Track: {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
+    },
+
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
     },
   },
 };
--- final/part-03/final/server/src/schema.js
+++ final/part-03/final/server/src/schema.js
@@ -1,20 +1,11 @@
-const { gql } = require("apollo-server");
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    "Get tracks array for homepage grid"
+    "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
     "Fetch a specific track, provided a track's ID"
-    track(id: ID!): Track
-  }
-
-  "A Module is a single unit of teaching. Multiple Modules compose a Track"
-  type Module {
-    id: ID!
-    "The Module's title"
-    title: String
-    "The Module's length in minutes"
-    length: Int
+    track(id: ID!): Track!
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -22,15 +13,15 @@ const typeDefs = gql`
     id: ID!
     "The track's title"
     title: String!
-    "The track's main author"
+    "The track's main Author"
     author: Author!
-    "The track's main illustration to display in track card or track page detail"
+    "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
-    "The track's complete description, can be in Markdown format"
+    "The track's complete description, can be in markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
@@ -43,8 +34,17 @@ const typeDefs = gql`
     id: ID!
     "Author's first and last name"
     name: String!
-    "Author's profile picture url"
+    "Author's profile picture"
     photo: String
+  }
+
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The module's title"
+    title: String!
+    "The module's length in minutes"
+    length: Int
   }
 `;
 
