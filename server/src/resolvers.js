14faae30a61484e0bb18d1970f01cf70998ecb4a
a-sh-dev <iam.angie@ymail.com>
2022-08-18 11:37:43 +1000

    Update TrackAPI & add a new resolver getTrackModules

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -16,6 +16,10 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
+
+    modules: ({ id }, _, { dataSources }) => {
+      return dataSources.trackAPI.getTrackModules(id);
+    },
   },
 };
 
